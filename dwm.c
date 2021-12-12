6613a90357ee61d7e692298a0bc77741b7f8723e
Varun Vasan V <varunvasan91@gmail.com>
2021-12-12 13:40:18 +0530

    center window title

--- dwm.c
+++ dwm.c
@@ -742,8 +742,11 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
+			/* Center align selected window name*/
+			int mid = (m->ww - (int)TEXTW(m->sel->name)) / 2 - x;
+			mid = mid >= lrpad / 2 ? mid : lrpad / 2;
 			drw_setscheme(drw, scheme[m == selmon ? SchemeNorm : SchemeSel]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
