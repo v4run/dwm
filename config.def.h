4f9870b6f009484eadca396b362663b2731b2e4f
Varun Vasan V <varunvasan91@gmail.com>
2021-12-26 21:17:08 +0530

    glyphs

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const unsigned int snap      = 1;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]    = { "FantasqueSansMono Nerd Font Mono:size=12" };
-static const char dmenufont[] = "DejaVu Sans Mono:size=11";
+static const char *fonts[]    = { "monospace:size=10", "FantasqueSansMono Nerd Font Mono:style=Regular:size=15" };
+static const char dmenufont[] = "monospace:size=11";
 
 static const char col_norm_bg[]      = "#040404";
 static const char col_norm_border[]  = "#000000";
@@ -34,7 +34,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,7 +42,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Signal",    NULL,       NULL,       1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
@@ -53,9 +54,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",      tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[﩯]",      tile },    /* first entry is default */
+	{ "[]",      NULL },    /* no layout function means floating behavior */
+	{ "[]",      monocle },
 };
 
 /* Volume buttons */
