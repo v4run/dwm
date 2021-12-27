35ce6f648d096774fedb745b3d6c2c68b8c48bdd
Varun Vasan V <varunvasan91@gmail.com>
2021-12-27 12:40:20 +0530

    helvum always float

--- config.def.h
+++ config.def.h
@@ -51,6 +51,7 @@ static const Rule rules[] = {
 	{ "Chromium",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Signal",    NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Music",     NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "helvum",    NULL,       NULL,            0,       1,           -1 },
 };
 
 /* layout(s) */
