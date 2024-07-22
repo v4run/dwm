53d43d4e4ae50ee67eab38620ead482500f3dae0
Varun Vasan V <varunvasan91@gmail.com>
2024-07-22 11:43:46 +0530

    win title color update

--- config.h
+++ config.h
@@ -27,7 +27,7 @@ static const char *colors[][3] = {
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
     [SchemeSel] = {col_gray1, col_cyan, col_cyan},
     [SchemeTitle] = {col_gray2, col_gray1, col_gray1},
-    [SchemeSelTitle] = {col_gray4, col_gray1, col_gray1},
+    [SchemeSelTitle] = {col_gray3, col_gray1, col_gray1},
     [SchemeSeparator] = {col_gray2, col_gray1, col_gray1},
 };
 
